d07f7fd7b59fe77002cde75b3617f15a6ccf0b79
cormac brady <cob16@aber.ac.uk>
2015-03-16 22:50:39 +0000

    Added level loader and started health

---
+++ level.2.js
@@ -0,0 +1,46 @@
+var boxes = [];
+
+// dimensions
+boxes.push({
+    x: 0,
+    y: 0,
+    width: 10,
+    height: height
+});
+boxes.push({
+    x: 0,
+    y: height - 2,
+    width: width,
+    height: 10
+});
+boxes.push({
+    x: width - 10,
+    y: 0,
+    width: 50,
+    height: height
+});
+
+boxes.push({
+    x: 120,
+    y: height - 20,
+    width: 80,
+    height: 80
+});
+boxes.push({
+    x: 170,
+    y: height - 30,
+    width: 80,
+    height: 80
+});
+boxes.push({
+    x: 220,
+    y: height - 20,
+    width: 80,
+    height: 80
+});
+boxes.push({
+    x: 270,
+    y: 150,
+    width: 40,
+    height: 40
+});
